4f0bfc9b5807068d08982a0c9605d67e056691c0
BiellEsdrass <gabriell.com.br13@gmail.com>
2023-02-05 02:40:11 -0300

    avengers

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 window.addEventListener('load', function() {
 	
 	// setTimeout to simulate the delay from a real page load
-	setTimeout(lazyLoad, 1000);
+	setTimeout(lazyLoad, 2000);
 	
 });
 
@@ -32,48 +32,4 @@ function lazyLoad() {
 	
 }
 
-
-const slider = document.querySelectorAll('.slider');
-const btnPrev = document.getElementById('prev-button');
-const btnNext = document.getElementById('next-button');
-
-let currentSlide = 0;
-
-function hideSlider() {
-	slider.forEach(item => item.classList.remove('on'))
-}
-
-function showSlider() {
-	slider[currentSlide].classList.add('on')
-}
-
-function nextSlider() {
-	hideSlider()
-		if(currentSlide === slider.length -1) {
-    currentSlide = 0
-} else {
-    currentSlide++
-}
-	showSlider()
-}
-
-function prevSlider() {
-	hideSlider()
-		if(currentSlide === 0) {
-    currentSlide = slider.length -1
-} else {
-    currentSlide--
-}
-	showSlider()
-}
-
-btnNext.addEventListener('click', nextSlider)
-btnPrev.addEventListener('click', prevSlider)
-
-
-console.log(slider)
-
-
-currentSlide = currentSlide + 1
-currentSlide = currentSlide - 1
 
